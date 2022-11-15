7b533581c3b9a6246628bdffe2b375517e12c3d2
Harsh Palan <harshpalan14@gmail.com>
2022-11-15 18:34:47 -0500

    Updated to 2D

--- WEEK10_HW1/sketch.js
+++ WEEK10_HW1/sketch.js
@@ -4,79 +4,88 @@ let defaultArtist = "Avicii";
 let defaultSong = "Wake me up";
 let graphics;
 
-let lyrics= '';
-
+let lyrics = "";
+let lyricsArray = [];
+let y = 200;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  
-  graphics = createGraphics(windowWidth,windowHeight*2);
-  
-  
+  angleMode(DEGREES);
+  createCanvas(windowWidth, windowHeight);
+
+  graphics = createGraphics(windowWidth, windowHeight * 2);
+
   artist = createInput(defaultArtist);
   artist.position(windowWidth / 4, windowHeight - 25);
-  artist.style('text-align','center');
+  artist.style("text-align", "center");
 
   button = createButton("Get Lyrics");
-  button.position(windowWidth / 2 , windowHeight - 25);
+  button.position(windowWidth / 2, windowHeight - 25);
   button.size(100, artist.height);
   button.style("border", "0px");
-  button.style("border-radius",  "10px");
-  button.style('text-align','center');
+  button.style("border-radius", "10px");
+  button.style("text-align", "center");
   button.mousePressed(searchSong);
-  
-  song = createInput(defaultSong);
-  song.position(windowWidth / 1.5 , windowHeight - 25);
-  song.style('text-align','center');
 
-  graphics.fill(random(255),random(255),random(255));
-  
-  // data1 = loadJSON(
-  //   "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" +
-  //   defaultArtist +
-  //   "&q_track=" +
-  //   defaultSong +
-  //   "&apikey=41ea63692fc0290c8f179fa97cf32389", getTrackId
-  // );
+  song = createInput(defaultSong);
+  song.position(windowWidth / 1.5, windowHeight - 25);
+  song.style("text-align", "center");
+
+  fill(random(255), random(255), random(255));
+
+  data1 = loadJSON(
+    "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" +
+    defaultArtist +
+    "&q_track=" +
+    defaultSong +
+    "&apikey=41ea63692fc0290c8f179fa97cf32389", getTrackId
+  );
+  frameRate(1);
 }
 
 function draw() {
   background(0);
-  
-  orbitControl();
-  graphics.textAlign(CENTER, TOP);
-  graphics.textSize(windowWidth/10);
-  graphics.text(lyrics, 0, 0, graphics.width, graphics.height);
-  translate(0,600 - millis()/ 50, millis()/ - 50);
-  noStroke();
-  texture(graphics);
-  // rotateX(PI/5);
-  plane(500, 600);
-  
-  
+  strokeWeight(0.5);
+  stroke(255);
+
+  push();
+  textSize(20);
+  translate(width/4 + random(-width/4,width/4), 50);
+  for (let i = 0; i < lyricsArray.length; i++) {
+    translate(10, 50);
+    rotate(-2 * random(-5, 5));
+    text(lyricsArray[i], -50, -20);
+    y = y + 15;
+  }
+  pop();
+  // noLoop();
 }
 
 function getTrackId(data1) {
   let trackId = data1.message.body.track_list[0].track.track_id;
-  console.log(trackId);
+  // console.log(trackId);
   data2 = loadJSON(
-    "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackId +
-    "&apikey=41ea63692fc0290c8f179fa97cf32389", getLyrics
+    "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" +
+      trackId +
+      "&apikey=41ea63692fc0290c8f179fa97cf32389",
+    getLyrics
   );
 }
 
 function getLyrics(data2) {
-  lyrics = lyrics.concat(data2.message.body.lyrics.lyrics_body);
-  lyrics = lyrics.slice(0, lyrics.indexOf('...'));
-  console.log(lyrics);
+  lyrics = data2.message.body.lyrics.lyrics_body;
+  lyrics = lyrics.slice(0, lyrics.indexOf("..."));
+  lyricsArray = lyrics.split("\n");
+  // console.log(lyricsArray);
 }
 
-function searchSong(){
+function searchSong() {
+  lyrics = "";
   data1 = loadJSON(
     "https://api.musixmatch.com/ws/1.1/track.search?q_artist=" +
-    artist.value() +
-    "&q_track=" +
-    song.value() +
-    "&apikey=41ea63692fc0290c8f179fa97cf32389", getTrackId
+      artist.value() +
+      "&q_track=" +
+      song.value() +
+      "&apikey=41ea63692fc0290c8f179fa97cf32389",
+    getTrackId
   );
 }
